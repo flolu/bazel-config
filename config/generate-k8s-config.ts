0143b76423c865ad7c5ab062338678fd719b96c8
flolu <loflude@gmail.com>
2020-11-21 08:57:18 +0100

    feat: generate kubernetes config maps

---
+++ config/generate-k8s-config.ts
@@ -0,0 +1,50 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as yaml from 'yaml'
+
+const ruleDir = process.argv[2]
+const configsDir = 'configs'
+
+function flattenObject(object: any) {
+  const toReturn = {}
+  for (const i in object) {
+    if (!object.hasOwnProperty(i)) continue
+    if (typeof object[i] === 'object' && object[i] !== null) {
+      const flatObject = flattenObject(object[i])
+      for (const x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue
+        ;(toReturn as any)[i + '.' + x] = (flatObject as any)[x]
+      }
+    } else {
+      ;(toReturn as any)[i] = object[i]
+    }
+  }
+  return toReturn
+}
+
+function k8sConfig(data: object) {
+  return {
+    apiVersion: 1,
+    kind: 'ConfigMap',
+    metadata: {
+      name: 'config',
+    },
+    data,
+  }
+}
+
+async function main() {
+  const files = await fs.promises.readdir(path.join(__dirname, configsDir))
+  await Promise.all(
+    files.map(async file => {
+      const configStr = await fs.promises.readFile(path.join(__dirname, configsDir, file))
+      const {$schema, ...config} = JSON.parse(configStr.toString())
+      const flat = flattenObject(config)
+      const content = yaml.stringify(k8sConfig(flat))
+      const outFile = `${file.split('.')[0]}.config.yaml`
+      await fs.promises.writeFile(path.join(ruleDir, outFile), content)
+    }),
+  )
+}
+
+main()
